1ab0125ea16976d084a21d281ec1e4757b6ac93e
jumphone <jumphone@163.com>
2016-11-05 03:08:53 -0500

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+from servercraft import *
+
+server_dir = "./"
+
+server_name = "server_center"
+
+app_name = 'app1'
+
+build_server(server_dir, server_name)
+
+add_app(server_dir ,app_name, server_name)
